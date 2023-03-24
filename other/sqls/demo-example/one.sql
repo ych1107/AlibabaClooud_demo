b46848256f7733238b294a049cbfe5dc834ca039
dftdla <1450180985@qq.com>
2023-03-24 11:16:28 +0800

    完善League_core V1.0

---
+++ other/sqls/demo-example/one.sql
@@ -0,0 +1,35 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80028
+ Source Host           : localhost:3306
+ Source Schema         : learn
+
+ Target Server Type    : MySQL
+ Target Server Version : 80028
+ File Encoding         : 65001
+
+ Date: 24/03/2023 10:53:02
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for one
+-- ----------------------------
+DROP TABLE IF EXISTS `one`;
+CREATE TABLE `one`  (
+  `id` int unsigned NOT NULL,
+  `sum` int unsigned NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of one
+-- ----------------------------
+INSERT INTO `one` VALUES (1, 3);
+
+SET FOREIGN_KEY_CHECKS = 1;
